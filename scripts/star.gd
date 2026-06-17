e88088edf94678cc32c6309f484312bc064048eb
Laboratório FIAP <labsfiap@fiap.com.br>
2026-06-17 11:25:42 -0300

    Pickups

--- scripts/star.gd
+++ scripts/star.gd
@@ -1,6 +1,6 @@
 extends Node2D
+@export var starID = 0
 @onready var player = get_tree().get_first_node_in_group("player")
-
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
@@ -8,11 +8,15 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
+	if starID in pickups.stars:
+		self.modulate.a = 0.2
+	else:
+		self.modulate.a = 1
 
 
 func _on_area_2d_body_entered(body: Node2D) -> void:
 	if body != player:
 		return
-	player.esta_invuln = false
-	player.morrer()
+	if starID not in pickups.stars:
+		pickups.stars.append(starID)
+		$AnimationPlayer.play('pickup')
